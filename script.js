e54aec5f107bb98e8fbd9a610c82ffb9ca464053
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-01-31 21:18:11 +0530

    remaster

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const tabs = document.getElementsByClassName("tabbut");
 const tabbar = document.querySelector(".ancbar");
 let at = 2;
 let displayed = 0;
-for (let i = 0; i < pads.length; i++) {
-    pads[i].style.transform = `translateX(${3*i-7}%) scaleX(1.05)`;
-}
-for (let i = 0; i < padns.length; i++) {
-    padns[i].style.transform = `translateX(${3*i-12}%) scaleX(1.05)`;
-}
-tabs[displayed].style.backgroundColor = "#007274";
+// for (let i = 0; i < pads.length; i++) {
+//     pads[i].style.transform = `translateX(${3*i-7}%) scaleX(1.05)`;
+// }
+// for (let i = 0; i < padns.length; i++) {
+//     padns[i].style.transform = `translateX(${3*i-12}%) scaleX(1.05)`;
+// }
+tabs[displayed].style.backgroundColor = "#6bb4a6";
 function swt(ind) {
 	txts[displayed].classList.remove("show");
-	tabs[displayed].style.backgroundColor = "#009da0";
-	tabs[ind].style.backgroundColor = "#007274";
+	tabs[displayed].style.backgroundColor = "#ba6c4b";
+	tabs[ind].style.backgroundColor = "#6bb4a6";
 	txts[ind].classList.add("show");
 	displayed = ind;
 }
