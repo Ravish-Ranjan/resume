6b6edf7e6ada4e9da9be874d76320f5b11baf3f7
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-02-07 11:21:22 +0530

    hamburger

--- files/script.js
+++ files/script.js
@@ -3,22 +3,33 @@ const padns = document.getElementsByClassName("padn");
 const txts = document.getElementsByClassName("infotxt");
 const tabs = document.getElementsByClassName("tabbut");
 const tabbar = document.querySelector(".ancbar");
+const nav = document.getElementById("nav");
 let at = 2;
 let displayed = 0;
 tabs[displayed].style.backgroundColor = "#6bb4a6";
 function swt(ind) {
 	txts[displayed].classList.remove("show");
 	tabs[displayed].style.backgroundColor = "#ba6c4b";
+
 	tabs[ind].style.backgroundColor = "#6bb4a6";
 	txts[ind].classList.add("show");
+
+	tabs[ind].scrollIntoView({inline:"center"})
 	displayed = ind;
+	shownav()
 }
 function scrollDiv(direction) {
 	const scrollAmount = 30;
-	const vw = window.innerWidth / 100;        
+	const vw = window.innerWidth / 100;
 	if (direction === "left") {
-	  tabbar.scrollLeft -= scrollAmount * vw;
+		tabbar.scrollLeft -= scrollAmount * vw;
 	} else if (direction === "right") {
-	  tabbar.scrollLeft += scrollAmount * vw;
+		tabbar.scrollLeft += scrollAmount * vw;
 	}
+}
+let tog = true;
+function shownav(open = tog) {
+	console.log(open);
+	nav.style.transform = (open)?"translateY(0%)":"translateY(-100%)";
+	tog = !tog;
 }
